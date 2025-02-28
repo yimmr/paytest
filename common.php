526a86a408810ac33edc16603be6abcf51a82e06
imoncn <service@yimmr.com>
2025-02-28 10:32:36 +0800

    first commit

---
+++ common.php
@@ -0,0 +1,26 @@
+<?php
+
+define('STORAGE_PATH', __DIR__.DIRECTORY_SEPARATOR.'storage');
+
+function storagePath(...$path)
+{
+    return implode(DIRECTORY_SEPARATOR, [STORAGE_PATH, ...$path]);
+}
+
+function saveRequest()
+{
+    $data = [
+        'time'    => time(),
+        'time'    => date('Y-m-d H:i:s'),
+        'ip'      => $_SERVER['REMOTE_ADDR'] ?? '',
+        'method'  => $_SERVER['REQUEST_METHOD'] ?? 'GET',
+        'url'     => $_SERVER['REQUEST_URI'] ?? '',
+        'referer' => $_SERVER['HTTP_REFERER'] ?? '',
+        'get'     => $_GET,
+        'post'    => $_POST,
+        'files'   => $_FILES,
+    ];
+    $json = json_encode($data);
+    file_put_contents(storagePath(date('Y-m-d_H-i-s').'.json'), $json);
+    return $data;
+}
---
+++ return.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__.'/common.php';
+
+saveRequest();
+
+echo 'Return';
